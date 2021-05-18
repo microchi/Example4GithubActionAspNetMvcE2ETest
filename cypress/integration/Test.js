a6274da2cc782391e5629527df7869998924650f
Timothy Chi <microchi@gmail.com>
2021-05-18 13:55:08 +0800

    Add Cypress Test

---
+++ cypress/integration/Test.js
@@ -0,0 +1,16 @@
+﻿it("visit home page", () => {
+    cy.visit("http://localhost:8080")
+    cy.contains("ASP.NET");
+})
+
+it("visit about page", () => {
+    cy.visit("http://localhost:8080")
+    cy.get('.navbar').contains("關於").click();
+    cy.contains("About");
+})
+
+it("visit about page", () => {
+    cy.visit("http://localhost:8080")
+    cy.get('.navbar').contains("連絡人").click();
+    cy.contains("Contact");
+})
